cc5a9cc7db2d46630573c750885a9d4ef1bb4869
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-05-23 09:57:08 +0530

    update

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -90,7 +90,7 @@ const Blog = () => {
 
         <div className="px-4 py-8">
           <div className="flex items-center justify-between mb-8">
-            <h1 className="text-3xl font-bold text-primary glow">blog posts</h1>
+            <h1 className="text-3xl font-bold text-primary glow">blogs</h1>
             <Button
               variant="outline"
               size="sm"
